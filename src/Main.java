96f21f59649ee5dca6a6a7b70859758e9216a332
arkmadj <ahmadjinadu@gmail.com>
2023-08-01 07:22:20 +0100

    added date variable

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import java.util.Date;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        byte age = 30;
-        long viewCount = 3_123_456_789L;
+        Date now = new Date();
+        System.out.println(now);
     }
 }
