5d8eb40adbc021ed5f61d38bcee9956a335f13c4
arkmadj <ahmadjinadu@gmail.com>
2023-08-01 08:52:12 +0100

    added tostring for array

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = new int[5];
-        numbers[0] = 1;
-        System.out.println(numbers);
+        int[] numbers = {2, 3, 5, 1, 4};
+        System.out.println(Arrays.toString(numbers));
 
     }
 }
