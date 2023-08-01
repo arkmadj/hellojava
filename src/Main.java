6feabeb3f5540aa7777871daf123bf15f57f283b
arkmadj <ahmadjinadu@gmail.com>
2023-08-01 13:22:39 +0100

    added Math class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {2, 3, 5, 1, 4};
-        System.out.println(Arrays.toString(numbers));
+        int result = (int)(Math.random() * 100);
+        System.out.println(result);
 
     }
 }
