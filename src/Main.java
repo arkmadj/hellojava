d41442b371f9ae3cdf6c4a8f109e4adbd2f4eec5
arkmadj <ahmadjinadu@gmail.com>
2023-08-02 14:26:28 +0100

    added if conditons

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,9 @@
-import java.text.NumberFormat;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
-
-        System.out.println("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-
-        System.out.println("Period (Years): ");
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEAR;
-
-        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)) / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage: " + mortgageFormatted);
+        int temp = 32;
+        if(temp > 30) {
+            System.out.println("It's a hot day");
+            System.out.println("Drink water");
+        }
     }
 }
